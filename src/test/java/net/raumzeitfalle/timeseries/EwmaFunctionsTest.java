cf5f283aa72ee1560344f80a4d0526392676b8d7
Oliver Loeffler <oliver.loeffler@amtc-dresden.com>
2016-10-11 18:29:48 +0200

    Boxing suppression removed.

--- src/test/java/net/raumzeitfalle/timeseries/EwmaFunctionsTest.java
+++ src/test/java/net/raumzeitfalle/timeseries/EwmaFunctionsTest.java
@@ -1,6 +1,8 @@
 package net.raumzeitfalle.timeseries;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -10,85 +12,82 @@ import java.util.stream.Collectors;
 
 import org.junit.Test;
 
-import net.raumzeitfalle.timeseries.EwmaFunctions;
-
-@SuppressWarnings("boxing")
 public class EwmaFunctionsTest {
 
-	@Test
-	public void getBiFunction() {
-		BiFunction<Double, Double, Double> ewma = EwmaFunctions.getStatelessBiFunction(0.2);
-		assertEquals( 0.56, ewma.apply(2.0, 0.2), 0.01);
-	}
-	
-	@Test
-	public void getFunction() {
-		Function<Double,Double> ewma = EwmaFunctions.withLambdaAndInitial(0.2,0.0);
-		assertEquals( 0.2, ewma.apply(1.0), 0.01);
-		assertEquals( 0.56, ewma.apply(2.0), 0.01);
-		assertEquals( 1.048, ewma.apply(3.0), 0.01);
-	}
-	
-	@Test
-	public void applyFunctionOnStream() {
-		LinkedList<Double> doubles = createLinkedListOfDoubles();
-		List<Double> results = doubles.stream().map( EwmaFunctions.get() ).collect( Collectors.toList() );
-		assertEquals( 0.0, results.get(0), 0.01);
-		assertEquals( 0.2, results.get(1), 0.01);
-		assertEquals( 0.56, results.get(2), 0.01);
-		assertEquals( 1.048, results.get(3), 0.01);
-	}
-	
-	@Test
-	public void applyToArrayOfDoublePrimitives() {
-		double[] values = new double[]{0.0,1.0,2.0,3.0};
-		double[] result = new EwmaFunctions().applyWithLambda(0.2, values);
-		double[] expect = new double[]{0.0,0.2,0.56,1.048};
-		assertArrayEquals(expect, result, 0.0001);
-	}
+    @Test
+    public void getBiFunction() {
+	BiFunction<Double, Double, Double> ewma = EwmaFunctions.getStatelessBiFunction(0.2);
+	assertEquals( 0.56, ewma.apply(Double.valueOf(2.0), Double.valueOf(0.2)).doubleValue(), 0.01);
+    }
 
-	private LinkedList<Double> createLinkedListOfDoubles() {
-		LinkedList<Double> doubles = new LinkedList<>();
-		doubles.add( Double.valueOf(0.0) );
-		doubles.add( Double.valueOf(1.0) );
-		doubles.add( Double.valueOf(2.0) );
-		doubles.add( Double.valueOf(3.0) );
-		return doubles;
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
+    @Test
+    public void getFunction() {
+	Function<Double,Double> ewma = EwmaFunctions.withLambdaAndInitial(0.2,0.0);
+	assertEquals( 0.2, ewma.apply(Double.valueOf(1.0)).doubleValue(), 0.01);
+	assertEquals( 0.56, ewma.apply(Double.valueOf(2.0)).doubleValue(), 0.01);
+	assertEquals( 1.048, ewma.apply(Double.valueOf(3.0)).doubleValue(), 0.01);
+    }
+
+    @Test
+    public void applyFunctionOnStream() {
+	LinkedList<Double> doubles = createLinkedListOfDoubles();
+	List<Double> results = doubles.stream().map( EwmaFunctions.get() ).collect( Collectors.toList() );
+	assertEquals( 0.0, results.get(0).doubleValue(), 0.01);
+	assertEquals( 0.2, results.get(1).doubleValue(), 0.01);
+	assertEquals( 0.56, results.get(2).doubleValue(), 0.01);
+	assertEquals( 1.048, results.get(3).doubleValue(), 0.01);
+    }
+
+    @Test
+    public void applyToArrayOfDoublePrimitives() {
+	double[] values = new double[]{0.0,1.0,2.0,3.0};
+	double[] result = new EwmaFunctions().applyWithLambda(0.2, values);
+	double[] expect = new double[]{0.0,0.2,0.56,1.048};
+	assertArrayEquals(expect, result, 0.0001);
+    }
+
+    private LinkedList<Double> createLinkedListOfDoubles() {
+	LinkedList<Double> doubles = new LinkedList<>();
+	doubles.add( Double.valueOf(0.0) );
+	doubles.add( Double.valueOf(1.0) );
+	doubles.add( Double.valueOf(2.0) );
+	doubles.add( Double.valueOf(3.0) );
+	return doubles;
+    }
+
+    @Test(expected=IllegalArgumentException.class)
     public void lambdaWithNaNValue() {
-		EwmaFunctions.getStatelessBiFunction(Double.NaN);
+	EwmaFunctions.getStatelessBiFunction(Double.NaN);
     }
 
     @Test(expected=IllegalArgumentException.class)
     public void lambdaWithInfinity() {
-    	EwmaFunctions.getStatelessBiFunction(Double.POSITIVE_INFINITY);
+	EwmaFunctions.getStatelessBiFunction(Double.POSITIVE_INFINITY);
     }
 
     @Test(expected=IllegalArgumentException.class)
     public void lambdaGreaterOne() {
-    	EwmaFunctions.getStatelessBiFunction(10);
+	EwmaFunctions.getStatelessBiFunction(10);
     }
 
     @Test(expected=IllegalArgumentException.class)
     public void lambdaEqualsZero() {
-    	EwmaFunctions.getStatelessBiFunction(0);
+	EwmaFunctions.getStatelessBiFunction(0);
     }
 
     @Test(expected=IllegalArgumentException.class)
     public void lambdaLessThanZero() {
-    	EwmaFunctions.getStatelessBiFunction(-1);
+	EwmaFunctions.getStatelessBiFunction(-1);
     }
-    
+
     @Test
     public void stringRepresentation(){
-    	assertEquals("EWMA_n = 0.35n + 0.65(n-1) with EWMA_0 = 2.0 and (n-1) = 2.0", EwmaFunctions.withLambdaAndInitial(0.35, 2.0).toString());
+	assertEquals("EWMA_n = 0.35n + 0.65(n-1) with EWMA_0 = 2.0 and (n-1) = 2.0", EwmaFunctions.withLambdaAndInitial(0.35, 2.0).toString());
     }
-    
+
     @Test
     public void instantiation() {
-    	assertNotNull( new EwmaFunctions() );
+	assertNotNull( new EwmaFunctions() );
     }
 
 }
